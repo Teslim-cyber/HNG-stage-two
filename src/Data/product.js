aa398a8d09e285776cfced539e85051a1632c0d1
Teslim-cyber <adelakunteslim@gmail.com>
2024-07-09 21:25:32 +0100

    Add files via upload

---
+++ src/Data/product.js
@@ -0,0 +1,52 @@
+
+export const data = [
+  {
+    id: 1,
+	image: require("../Images/image 14.png"),
+	description: "Oleves Dress watch with Large Easy-analog",
+	price: "#45000"
+  },
+  {
+    id: 2,
+	image: require("../Images/image 15.png"),
+	description: "Oleves watch Diamond Business Dress Analog Quartx",
+	price: "#55000"
+  },
+  {
+    id: 3,
+	image: require("../Images/image 16.png"),
+	description: "Oleves Men's Watch Analog Quartz Movement Business Stanless Stee",
+	price: "#65000"
+  },
+  {
+    id: 4,
+	image: require("../Images/image 17.png"),
+	description: "Men Watch Top Berana Original Sports Quartz Mens Watches Full",
+	price: "#95000"
+  },
+   {
+    id: 5,
+	image: require("../Images/image 18.png"),
+	description: "IPC Men watch stainless steel luxury Hidden clasp button waterproof luminos date  ",
+	price: "#55000"
+  },
+   {
+    id: 6,
+	image: require("../Images/image 19.png"),
+	description: "OLEVS Watch Diamond Business Dress Analog Quartz",
+	price: "#35000"
+  },
+   {
+    id: 7,
+	image: require("../Images/image 20.png"),
+	description: "BELUSHI 556 Full steel band business style calendar quartz",
+	price: "#45000"
+  },
+   {
+    id: 8,
+	image: require("../Images/image 21.png"),
+	description: "IPC Men watch top grade watch classic roamn numeral Dial",
+	price: "#65000"
+  },
+  
+]
